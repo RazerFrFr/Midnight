ede2757fdeace2a69131772d2975442d26e48405
rumblechaosgame-tech <rumblechaosgame@gmail.com>
2026-07-25 13:48:45 +0100

    Update BACKEND_URL to new IP address

--- includes/opts.h
+++ includes/opts.h
@@ -1,6 +1,6 @@
 #pragma once
 
-constexpr const char *BACKEND_URL = "http://192.168.1.69:8080";
+constexpr const char *BACKEND_URL = "http://82.23.183.23:8080";
 static const char* EPIC_DOMAINS[] = {
     "game-social.epicgames.com",
     "ol.epicgames.com",
